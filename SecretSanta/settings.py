005391e49c802e547cbd90c115e031131db97c57
Your Name <you@example.com>
2019-12-24 10:08:38 +0400

    commit

--- SecretSanta/settings.py
+++ SecretSanta/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'oj%31y#21kx&@ptkn7!uh&cdu5n-twfxrejc!9x76_rgin-1-r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://secretsantaweb.herokuapp.com/"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
